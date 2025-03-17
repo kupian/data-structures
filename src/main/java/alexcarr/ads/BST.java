dd80bdc0a7a019d4cab32e54f8413e049df3b55c
Alex Carr <alexcarr1113@outlook.com>
2025-03-17 10:49:38 +0000

    updated methods

--- src/main/java/alexcarr/ads/BST.java
+++ src/main/java/alexcarr/ads/BST.java
@@ -1,35 +1,50 @@
 package alexcarr.ads;
 
 
+import java.util.NoSuchElementException;
+
 public class BST {
     private Node root;
 
     private class Node {
-        private final int key;
+        private int key;
         private Node left, right, p;
         private int size;
 
         public Node(int key, int size) {
-            this.key = key;
             this.size = size;
+            this.key = key;
             this.left = null;
             this.right = null;
             this.p = null;
         }
     }
 
-    public BST(Node root) {
-        this.root = root;
+    public BST() {
+        root = null;
     }
 
-    public static Node max(Node x) {
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.size;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private Node max(Node x) {
         while (x.left != null) {
             x = x.left;
         }
         return x;
     }
 
-    public static Node min(Node x) {
+    private Node min(Node x) {
         while (x.right != null) {
             x = x.right;
         }
@@ -37,10 +52,12 @@ public class BST {
     }
 
     public Node max() {
+        if (isEmpty()) throw new NoSuchElementException("Tree is empty");
         return max(root);
     }
 
     public Node min() {
+        if (isEmpty()) throw new NoSuchElementException("Tree is empty");
         return min(root);
     }
 
@@ -56,7 +73,7 @@ public class BST {
         return search(root, key);
     }
 
-    public static Node successor(Node x) {
+    public Node successor(Node x) {
         if(x == null) return null;
         if (x.right != null) return min(x.right);
 
@@ -68,8 +85,8 @@ public class BST {
         return y;
     }
 
-    public static Node predecessor(Node x) {
-        if(x == null) return null;
+    public Node predecessor(Node x) {
+        if (x == null) return null;
         if (x.left != null) return max(x.left);
         Node y = x.p;
         while (y != null && x == y.left) {
@@ -79,19 +96,14 @@ public class BST {
         return y;
     }
 
-    public static int size(Node x) {
-        if(x == null) return 0;
-        return size(x.left) + size(x.right) + 1;
-    }
-
-    public static int height(Node x) {
+    public int height(Node x) {
         if(x == null) return 0;
         if(x.left == null && x.right == null) return 0;
         return Math.max(height(x.left), height(x.right)) + 1;
     }
 
-    public static void insert(BST T, Node z) {
-        Node x = T.root;
+    public void insert(Node z) {
+        Node x = root;
         Node y = null;
         while (x != null) {
             y = x;
@@ -104,7 +116,7 @@ public class BST {
         }
         z.p = y;
         if (y == null) {
-            T.root = z;
+            root = z;
         }
         else if (z.key < y.key) {
             y.left = z;
@@ -114,45 +126,45 @@ public class BST {
         }
     }
 
-    public static void transplant(BST T, Node u, Node v) {
-        if (u.p == null) T.root = v;
+    public void transplant(Node u, Node v) {
+        if (u.p == null) root = v;
         else if (u == u.p.left) u.p.left = v;
         else u.p.right = v;
 
         if(v != null) v.p = u.p;
     }
 
-    public static void delete(BST T, Node z) {
-        if (z.left == null) transplant(T, z, z.right);
-        else if (z.right == null) transplant(T, z, z.left);
+    public void delete(Node z) {
+        if (z.left == null) transplant(z, z.right);
+        else if (z.right == null) transplant(z, z.left);
         else {
             Node y = min(z.right);
             if (y.p != z) {
-                transplant(T, z, y.right);
+                transplant(z, y.right);
                 y.right = z.right;
                 y.right.p = y;
             }
-            transplant(T, z, y);
+            transplant(z, y);
             y.left = z.left;
             y.left.p = y;
         }
     }
 
-    public static void inorder(Node T) {
+    public void inorder(Node T) {
         if (T == null) return;
         inorder(T.left);
         System.out.println(T.key);
         inorder(T.right);
     }
 
-    public static void preorder(Node T) {
+    public void preorder(Node T) {
         if (T == null) return;
         System.out.println(T.key);
         preorder(T.left);
         preorder(T.right);
     }
 
-    public static void postorder(Node T) {
+    public void postorder(Node T) {
         if (T == null) return;
         postorder(T.left);
         postorder(T.right);
@@ -163,11 +175,11 @@ public class BST {
         return checkBST(root);
     }
 
-    public static boolean checkBST(Node T) {
+    public boolean checkBST(Node T) {
         return inRangeBST(T, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    public static boolean inRangeBST(Node T, int low, int high) {
+    public boolean inRangeBST(Node T, int low, int high) {
         if (T == null) return true;
 
         if (T.key < low || T.key > high) return false;
